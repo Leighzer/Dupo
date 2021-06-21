05499da6b7d47504c001ee5dd6a844f7d70c2472
Leighzer <bwc_nothgiel@live.com>
2021-06-21 01:59:49 -0400

    List files grouped by md5 hash of contents;

--- Dupo/Program.cs
+++ Dupo/Program.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Dupo
 {
-    // interesting starter code getting all files (and files in subdirectories) in a directory
-    // https://stackoverflow.com/questions/929276/how-to-recursively-list-all-the-files-in-a-directory-in-c/929418#929418
     public class Program
     {
         public static void Main(string[] args)
@@ -16,10 +16,47 @@ namespace Dupo
                 string path = args[0];
                 if (Directory.Exists(path))
                 {
-                    var test = GetFiles(path).ToList();
-                    foreach (string file in GetFiles(path))
+                    IEnumerable<string> files = GetFiles(path);
+
+                    Console.WriteLine($"{files.Count()} file(s) found.");
+
+                    Dictionary<string, List<string>> hashLookup = new Dictionary<string, List<string>>();
+
+                    using (var md5 = MD5.Create())
                     {
-                        Console.WriteLine(file);
+                        foreach (var file in files)
+                        {
+                            using (FileStream stream = File.OpenRead(file))
+                            {
+                                byte[] hash = md5.ComputeHash(stream);
+                                string hashString = BytesToString(hash);
+
+                                if (!hashLookup.ContainsKey(hashString))
+                                {
+                                    hashLookup.Add(hashString, new List<string>() { file });
+                                }
+                                else
+                                {
+                                    hashLookup[hashString].Add(file);
+                                }
+                            }
+                        }
+                    }
+
+                    int dupeCount = hashLookup.Where(x => x.Value.Count > 1).Count();
+
+                    Console.WriteLine($"{dupeCount} set(s) of files with same contents found. (md5 hash of contents match)" + Environment.NewLine);
+
+                    var orderedKeyValues = hashLookup.OrderByDescending(x => x.Value.Count).ToList();
+
+                    foreach (var pair in orderedKeyValues)
+                    {
+                        Console.WriteLine($"{pair.Value.Count} {pair.Key}");
+                        foreach (var file in pair.Value)
+                        {
+                            Console.WriteLine(file);
+                        }
+                        Console.WriteLine(Environment.NewLine);
                     }
                 }
                 else
@@ -33,6 +70,20 @@ namespace Dupo
             }
         }
 
+        // adapted from: http://csharphelper.com/blog/2018/04/calculate-hash-codes-for-a-file-in-c/
+        public static string BytesToString(byte[] bytes)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+            string result = sb.ToString();
+            return result;
+        }
+
+        // interesting code getting all files (and files in subdirectories) in a directory
+        // https://stackoverflow.com/questions/929276/how-to-recursively-list-all-the-files-in-a-directory-in-c/929418#929418
         public static IEnumerable<string> GetFiles(string path)
         {
             Queue<string> queue = new Queue<string>();
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Dupo
+A program that finds all files under a directory (including subdirectories), computes an md5 hash of the contents of each file, and finally lists the files grouped by hash.
+
+This tool is to help assist in identifying duplicate files within a directory so the user can better organize their files and reduce wasted hard drive space.
+
+# Usage
+Dupo /path/to/directory
+
+## Stretch Goal
+Instead of dumping a list, an interactive mode could be useful. Go over the files group by group, and let the user specify what to delete and keep. Maybe even a copyto function, where the original directory is untouched and a new directory is made with files copied from the original - but without duplicates. Ideas.
